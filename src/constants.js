e995bef0bbd9a7c832eb5cfc61896f9e37e5735c
SnowyField <aswdqe1x@gmail.com>
2022-09-26 22:17:25 +0700

    change color

--- src/constants.js
+++ src/constants.js
@@ -7,23 +7,23 @@ export const pixelConfig = {
         addition: 'rounded-sm w-[87%] h-4 cursor-pointer',
     },
     1: {
-        bg: 'bg-rose-500 hover:bg-rose-400',
+        bg: 'bg-rose-500 hover:bg-rose-600',
         addition: 'rounded-sm w-[87%] h-4 cursor-pointer',
     },
     2: {
-        bg: 'bg-violet-500 hover:bg-violet-400',
+        bg: 'bg-violet-500 hover:bg-violet-600',
         addition: 'rounded-sm w-[87%] h-4 cursor-pointer',
     },
     3: {
-        bg: 'bg-amber-500 hover:bg-amber-400',
+        bg: 'bg-sky-500 hover:bg-sky-600',
         addition: 'rounded-sm w-[87%] h-4 cursor-pointer',
     },
     4: {
-        bg: 'bg-sky-500 hover:bg-sky-400',
+        bg: 'bg-amber-500 hover:bg-amber-600',
         addition: 'rounded-sm w-[87%] h-4 cursor-pointer',
     },
     5: {
-        bg: 'bg-emerald-500 hover:bg-emerald-400',
+        bg: 'bg-emerald-500 hover:bg-emerald-600',
         addition: 'rounded-sm w-[87%] h-4 cursor-pointer',
     },
 
@@ -37,23 +37,23 @@ export const pixelConfig = {
 export const moodColors = {
     1: {
         fill: 'fill-rose-500',
-        hover: 'hover:fill-rose-500',
+        hover: 'hover:fill-rose-600',
     },
     2: {
         fill: 'fill-violet-500',
-        hover: 'hover:fill-violet-500',
+        hover: 'hover:fill-violet-600',
     },
     3: {
-        fill: 'fill-amber-500',
-        hover: 'hover:fill-amber-500',
+        fill: 'fill-sky-500',
+        hover: 'hover:fill-sky-600',
     },
     4: {
-        fill: 'fill-sky-500',
-        hover: 'hover:fill-sky-500',
+        fill: 'fill-amber-500',
+        hover: 'hover:fill-amber-600',
     },
     5: {
         fill: 'fill-emerald-500',
-        hover: 'hover:fill-emerald-500',
+        hover: 'hover:fill-emerald-600',
     },
 
     addition: 'group relative inline-block cursor-pointer',
