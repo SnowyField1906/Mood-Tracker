6ea2c846b349c228f7b95020643a19de7bc636c0
SnowyField <aswdqe1x@gmail.com>
2022-09-26 16:50:35 +0700

    add tooltip

--- src/svgs/Tough.js
+++ src/svgs/Tough.js
@@ -82,7 +82,7 @@ function Tough({ mood, setMood }) {
             </svg>
 
             <div id="tooltip-bottom-1" role="tooltip" class="inline-block absolute invisible z-10 py-2 px-3 text-sm font-medium text-white bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip dark:bg-gray-700">
-                Amazing
+                Tough
                 <div class="tooltip-arrow" data-popper-arrow></div>
             </div>
 
